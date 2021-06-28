7f2c0136f357d0b98dcfef539d800ae62b75adb4
Jan Tam <jsc_tam@hotmail.co.nz>
2021-06-28 17:07:46 +1200

    Initialize project using Create React App

---
+++ src/features/cart/cartSlice.js
@@ -0,0 +1,12 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+export const cartSlice = createSlice({
+  name: 'counter',
+  initialState: {},
+  reducers: {}
+})
+
+// Action creators are generated for each case reducer function
+export const {} = cartSlice.actions
+
+export default cartSlice.reducer
