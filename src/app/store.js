653bcee54f3f0660962f8b6a0f38260e29fb8a02
Jan Tam <jsc_tam@hotmail.co.nz>
2021-06-29 21:47:53 +1200

    Added redux persist to store state between refreshes

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,38 @@
-import { configureStore } from '@reduxjs/toolkit'
-import cartReducer from '../components/cart/cartSlice'
+import cartReducer from "../components/cart/cartSlice";
+import { combineReducers } from "redux";
+import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage"; // defaults to localStorage for web
 
-export default configureStore({
-  reducer: {
-      cart: cartReducer
-  }
-})
+const persistConfig = {
+  key: "root",
+  storage,
+};
+
+//Creates a root reducer for persistReducer
+const reducers = combineReducers({
+  cart: cartReducer,
+});
+
+const persistedReducer = persistReducer(persistConfig, reducers);
+
+export const store = configureStore({
+  reducer: persistedReducer,
+  middleware: getDefaultMiddleware({
+    serializableCheck: {
+      // https://github.com/rt2zz/redux-persist/issues/988#issuecomment-552242978
+      ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+    },
+  }),
+});
+
+export const persistor = persistStore(store);
