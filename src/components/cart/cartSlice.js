0d6c150a3a130326a1f46adb532025e432c14a75
Jan Tam <jsc_tam@hotmail.co.nz>
2021-06-29 16:15:12 +1200

    Added increment quantity if duplicate item added to cart functionality

--- src/components/cart/cartSlice.js
+++ src/components/cart/cartSlice.js
@@ -7,16 +7,24 @@ export const cartSlice = createSlice({
   },
   reducers: {
     addToCart: (state, action) => {
+      const addedItem = action.payload;
+      
+      // Search if the item we're wanting to add already exists in cart
       const itemExists = state.cartItems.find(
-        (item) => item.name === action.payload.name
+        (item) => item.name === addedItem.name
       );
 
+      // Adds the new item to cart only if item isn't already in cart. Otherwise increases item quantity by 1.
       if (itemExists) {
-        console.log("Item already in cart.");
+        // Finds the existing item in cart state and increments its quantity by 1.
+        let foundIndex = state.cartItems.findIndex(
+          (x) => x.name === addedItem.name
+        );
+        state.cartItems[foundIndex].quantity++;
       } else {
-        let newCartItem = action.payload
-        newCartItem.quantity = 1
-        state.cartItems.push(newCartItem);
+        // Add new item to cart state
+        addedItem.quantity = 1;
+        state.cartItems.push(addedItem);
       }
     },
   },
